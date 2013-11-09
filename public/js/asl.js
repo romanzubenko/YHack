14cdb8265e381dade642e4812c9cbcc204087ac2
Hiroki Osame <hiroki.osame@gmail.com>
2013-11-09 16:55:17 -0500

    ASL files

--- public/js/asl.js
+++ public/js/asl.js
@@ -1,25 +1,38 @@
-$(document).on('click',"#asl",run)
 
-name = 'aaRoN';
+
+$(document).on('click', "a[asl]", function(e){
+
+
+
+	var queue = [];
+
+	var word = $(e.target).attr("asl").replace(/\W/g, '').toLowerCase();
+	for(char c : word.toCharArray()) {
+		console.log(c);
+	}
+
+
+})
+
 name = name.toLowerCase()
-var alphabet =new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+var alphabet = new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 //console.log('alph',alphabet)
 alp = alphabet;
 word_loc = -1;
 videoPlayer = document.getElementById("homevideo");
 
 function run(){
-        word_loc++;
-        alphLoc = getLetterIndex(word_loc);
-        if (word_loc == name.length) window.location = "http://localhost:3000";
-        var nextVideo = "video/"+alp[alphLoc]+".mp4";
-        videoPlayer.src = nextVideo;
-        videoPlayer.play();
-   };
+				word_loc++;
+				alphLoc = getLetterIndex(word_loc);
+				if (word_loc == name.length) window.location = "http://localhost:3000";
+				var nextVideo = "video/"+alp[alphLoc]+".mp4";
+				videoPlayer.src = nextVideo;
+				videoPlayer.play();
+	 };
 
 function getLetterIndex(a) {
-  var letter = name.charAt(a)
-  var loc = alp.indexOf(letter);
-  console.log(loc)
-  return loc
+	var letter = name.charAt(a)
+	var loc = alp.indexOf(letter);
+	console.log(loc)
+	return loc
 }
